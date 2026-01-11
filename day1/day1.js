31ca580c8f7df783a4c6f718662e89c40ae7cae9
Mr-JsJ <jsjjithinsjohn@gmail.com>
2026-01-11 14:22:31 +0530

    Day1 of typescript

---
+++ day1/day1.js
@@ -0,0 +1,21 @@
+console.log("HELLO WORLD");
+var a = 10;
+console.log(a);
+var b = "hello"; ///in type script type is imp ,we can manually give the type of var
+console.log(b);
+var c = 10;
+console.log(c);
+c = "hello man";
+console.log(c.toUpperCase());
+console.log(c);
+//any it will not check the type before execution but unknown will check if type is known
+var d = "hello shejin";
+console.log(d);
+d = 10;
+console.log(d);
+var e;
+console.log(e);
+e = 20;
+console.log(e);
+var f = null;
+console.log(f);
---
+++ day1/day1.ts
@@ -0,0 +1,34 @@
+console.log("HELLO WORLD");
+
+
+let a =10;
+console.log(a);
+
+let b:string="hello";  ///in type script type is imp ,we can manually give the type of var
+console.log(b);
+
+let c:any=10;
+console.log(c);
+c="hello man";
+console.log(c.toUpperCase());
+console.log(c);
+
+
+//any it will not check the type before execution but unknown will check if type is known
+
+let d:unknown="hello shejin";
+console.log(d);
+d=10;    
+console.log(d);
+
+let e;
+console.log(e);
+e=20;
+console.log(e);
+
+let f:null =null;
+console.log(f);
+
+
+
+
